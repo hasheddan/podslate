b335c6a02cc85d1d62028dd759d787334a9077d1
hasheddan <georgedanielmangum@gmail.com>
2020-09-12 14:16:50 -0500

    Add initial pedestal setup
    Adds pedestal dependencies and implements initial server entrypoint.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- project.clj
+++ project.clj
@@ -2,7 +2,11 @@
   :description "The universal podcast URL translator"
   :license {:name "APACHE LICENSE, VERSION 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.route "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [org.slf4j/slf4j-simple "1.7.28"]]
   :main ^:skip-aot podslate.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
